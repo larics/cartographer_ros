e28ff948e67889a1f19c1abeec91d1221adece3c
Lovro Markovic <lovro.markovic@fer.hr>
2021-06-29 18:06:45 +0200

    [metrics_test] Add main

--- cartographer_ros/cartographer_ros/metrics/internal/metrics_test.cc
+++ cartographer_ros/cartographer_ros/metrics/internal/metrics_test.cc
@@ -102,3 +102,9 @@ TEST(Metrics, HistogramScaledPowersOfTest) {
 
 }  // namespace metrics
 }  // namespace cartographer_ros
+
+int main(int argc, char** argv) {
+  srand(time(NULL));
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
